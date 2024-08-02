81eefaeb7911745bbfc433caa8e4acb57329a50c
Feng Zhang <1666870846@qq.com>
2024-08-02 16:33:38 +0800

    第二部分

--- enemies/carb/carb_megablock_action.gd
+++ enemies/carb/carb_megablock_action.gd
@@ -22,7 +22,6 @@ func perform_action()->void:
 	block_effect.sfx = sfx
 	block_effect.execute([enemy])
 	
-	
 	get_tree().create_timer(0.6, false).timeout.connect(
 		func():
 			already_used = true
